eae8a3f53bc64c70960199d3900eaf22eb89cf56
Rocco Bashir DellAmico <rocco.dellamico@globant.com>
2025-12-12 02:49:50 -0300

    agrego funcion para consulta a la base de datos en metrics.model

--- backend/src/models/metrics.model.js
+++ backend/src/models/metrics.model.js
@@ -1,5 +1,14 @@
+const db = require("../config/db");
 
+const getUserMetrics = async (id) => {
+    const query = "SELECT last_login, login_count FROM users WHERE id = ?";
+
+    const queryResult = await db.query(query, [id]);
+    const result = queryResult[0];
+
+    return result;
+};
 
 module.exports = {
-    
-}
+    getUserMetrics,
+};
