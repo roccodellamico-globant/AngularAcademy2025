7c4ffa21dd6eb5c3c9ae4ee033d3bb3d6ed408dd
Rocco Bashir DellAmico <rocco.dellamico@globant.com>
2025-12-12 02:47:04 -0300

    agrego funciones para conultas a la base de datos en el auth.model

--- backend/src/models/auth.model.js
+++ backend/src/models/auth.model.js
@@ -1,3 +1,5 @@
+const db = require("../config/db");
+
 const validEmail = (email) => {
   return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
 };
@@ -20,9 +22,48 @@ const missingPropertyLogin = (email, password) => {
   return false;
 }
 
+const findUserByEmail = async (email, callback) => {
+  const query = "SELECT * FROM users WHERE email = ?";
+
+  const queryResult = await db.query(query, [email]);
+  const rows = queryResult[0];
+
+  return rows.length > 0 ? rows[0] : null;
+};
+
+const registerUser = async ([name, email, hashedPassword]) => {
+  const query = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+
+  const queryResult  = await db.query(query, [name, email, hashedPassword]);
+  const result = queryResult[0]
+
+  return result;
+}
+
+const updateMetrics = async (id) => {
+  console.log("Adentro")
+  const query = "UPDATE users SET last_login = NOW(), login_count = login_count + 1 WHERE id = ?";
+
+  await db.query(query, [id]);
+  console.log("listo")
+};
+
+const me = async (id) => {
+  const query = "SELECT id, name, email, last_login, login_count FROM users WHERE id = ?"
+
+  const queryResult = await db.query(query, [id]);
+  const result = queryResult[0];
+
+  return result;
+};
+
 module.exports = {
   validEmail,
   minimumPsw,
   missingPropertyRegister,
   missingPropertyLogin,
+  findUserByEmail,
+  registerUser,
+  updateMetrics,
+  me,
 };
