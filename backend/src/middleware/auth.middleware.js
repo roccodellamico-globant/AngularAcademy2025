2290ee5b269e18ac36e74d38f15130855920f97f
Rocco Bashir DellAmico <rocco.dellamico@globant.com>
2025-12-12 02:45:32 -0300

    modificacion del middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,29 +1,24 @@
-// Importar modulos
 const jwt = require("jsonwebtoken");
 
-const authMiddleWare = (req, res, next) => {
-    const token = req.headers.authorization
+const authMiddleware = (req, res, next) => {
+    const token = req.headers.authorization;
 
-    if( !token ){
+    if (!token) {
         return res.status(401).json({
-            message: "La peticion no tiene la cabezera de autenticacion"
-        })
-    };
+            message: "La peticion no tiene la cabecera de autenticacion"
+        });
+    }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        // Agregar datos de usuario a la requets { id, email }
         req.user = decoded;
-
-    } catch( error ) {
+    } catch (error) {
         return res.status(401).json({
             message: "Token invalido"
         });
     }
 
-    // Pasara a ejecucion de accion
-    next()
+    next();
 };
 
-module.exports = { authMiddleWare };
+module.exports = { authMiddleware };
