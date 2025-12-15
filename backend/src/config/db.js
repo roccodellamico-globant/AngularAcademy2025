e19663cee6c435c847cab5199d5f1679b2b0759b
Rocco Bashir DellAmico <rocco.dellamico@globant.com>
2025-12-15 16:03:26 -0300

    elimino comentarios en db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,7 +8,7 @@ dotenv.config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS, // Corregir variable de entorno
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
